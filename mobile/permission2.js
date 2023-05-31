a0161da0217e4b930c11fa9c314e165a78920ff1
Kartik Saxena <mailtokartiksaxena@gmail.com>
2023-05-31 13:03:54 +0530

    adding files

---
+++ mobile/permission2.js
@@ -0,0 +1,23 @@
+import { PermissionsAndroid } from 'react-native';
+
+async function hasCameraPermission() {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: 'Camera Permission',
+        message: 'This app needs access to your camera.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('Camera permission granted');
+    } else {
+      console.log('Camera permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
